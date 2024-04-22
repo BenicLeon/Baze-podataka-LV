26f6c7bd247bc93d1aa44916d35b66b6e702280d
LeonBenic <64423174+Leeonbenaa@users.noreply.github.com>
2024-04-22 20:22:47 +0200

    Update and rename LV3.txt to LV3.sql

--- LV3.sql
+++ LV3.sql
@@ -6,25 +6,25 @@ ime varchar(30),
 prezime varchar(30),
 jmbg char(13) primary key,
 datum_rod datetime,
-spol char(1) default('Ž'),
+spol char(1) default('Å½'),
 visina dec(5,2),
 slika image,
 broj_cipela smallint,
-CONSTRAINT spol_Tina CHECK(spol IN('M','Ž'))
+CONSTRAINT spol_Tina CHECK(spol IN('M','Å½'))
 );
 
 SET DATEFORMAT dmy
 
 INSERT INTO osobe_Tina (ime, prezime, jmbg, datum_rod, spol, visina, slika, broj_cipela)
-VALUES ('Ana','Aniæ','1234567891254','06/01/1987','Ž',163,'',39)
+VALUES ('Ana','AniÃ¦','1234567891254','06/01/1987','Å½',163,'',39)
 INSERT INTO osobe_Tina (ime, prezime, jmbg, datum_rod, spol, visina, slika, broj_cipela)
-VALUES ('Marko','Periæ','1234545891254','08/02/1995','M',180,'',43)
+VALUES ('Marko','PeriÃ¦','1234545891254','08/02/1995','M',180,'',43)
 INSERT INTO osobe_Tina (ime, prezime, jmbg, datum_rod, spol, visina, slika, broj_cipela)
-VALUES ('Jasna','Janiæ','1234567891114','05/11/1990','Ž',173,'',41)
+VALUES ('Jasna','JaniÃ¦','1234567891114','05/11/1990','Å½',173,'',41)
 INSERT INTO osobe_Tina (ime, prezime, jmbg, datum_rod, spol, visina, slika, broj_cipela)
 VALUES ('Minja','Klepetalo','4444567891254','18/05/1994','M',195,'',46)
 INSERT INTO osobe_Tina (ime, prezime, jmbg, datum_rod, spol, visina, slika, broj_cipela)
-VALUES ('Mateja','Zeliæ','5555567891254','23/04/1992','Ž',164,'',38)
+VALUES ('Mateja','ZeliÃ¦','5555567891254','23/04/1992','Å½',164,'',38)
 
 SELECT * FROM osobe_Tina
 
@@ -32,7 +32,7 @@ SELECT * FROM osobe_Tina
 
 
 UPDATE osobe_Tina SET
-ime='Periæ',
+ime='PeriÃ¦',
 prezime='Marko'
 WHERE jmbg='1234545891254';
 
@@ -45,7 +45,7 @@ DELETE FROM osobe_Tina WHERE ime='Jasna';
 Ispisati sve podatke o studentima prema uzlaznom 
 redoslijedu datuma upisa na fakultet. Za atribute 
 koristiti opisne nazive (AS). Ispisati ime i prezime
-svih studenata koji su roðeni prije 01.01.1978. godine.
+svih studenata koji su roÃ°eni prije 01.01.1978. godine.
 
 
 SELECT * FROM student ORDER BY god_upis ASC
@@ -54,7 +54,7 @@ SELECT ime_stud AS 'ime',prez_stud AS 'prezime' FROM student where dat_rod<'01.0
 
 
 Ispisati ime i prezime studenta i naziv predmeta za 
-sve one ispite koji su položeni s ocjenom 4 ili veæom.
+sve one ispite koji su poloÅ¾eni s ocjenom 4 ili veÃ¦om.
 
 SELECT p.naz_pred AS kolegij, s.ime_stud AS ime, s.prez_stud AS prezime
 FROM student AS s, predmet AS p, ispit AS i
@@ -63,7 +63,7 @@ p.sif_pred = i.sif_pred AND
 i.ocjena>=4
 
 Ispisati sve podatke iz tablice mjesto poredano uzlazno 
-prema poštanskom broju.
+prema poÅ¡tanskom broju.
 
 SELECT * FROM mjesto order by pbr ASC
 
